96a75c26a271e741fa8638c194652117e40a5213
UdayMayanna <mayannauday9@gmail.com>
2026-06-25 02:32:23 +0530

    Updated Hr Services
    Signed-off-by: UdayMayanna <mayannauday9@gmail.com>

--- src/app/services/hr-service.service.ts
+++ src/app/services/hr-service.service.ts
@@ -7,17 +7,18 @@ import { Hr } from '../models/Hr';
 })
 export class HrServiceService {
 
+  private baseUrl = "https://hr-employee-management-backend.onrender.com";
   constructor(private http:HttpClient){}
 
   registerHR(Data:Hr){ 
-    return this.http.post("http://localhost:8080/hr/",Data);
+    return this.http.post(this.baseUrl+"/hr/",Data);
   }
 
    loginHr(email : string,password : string){
-    return this.http.get<Hr>("http://localhost:8080/hr/"+email+"/"+password);
+    return this.http.get<Hr>(this.baseUrl+"/hr/"+email+"/"+password);
    }
 
    updateHr(Data:Hr,id:number){
-    return this.http.put("http://localhost:8080/hr/"+id,Data);
+    return this.http.put(this.baseUrl+"/hr/"+id,Data);
    }
 }
